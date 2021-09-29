1958b3c6d3232a2a754e880564816218d2732cd3
rahimin10 <rahimin710@gmail.com>
2021-09-29 23:35:09 +0700

    Initial commit

---
+++ Assets/_Complete-Game/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;           
+    public float smoothing = 5f;        
+
+    Vector3 offset;                     
+
+    private void Start ()
+   {
+        //mendapatkan offset antara target dan kamera
+        offset = transform.position - target.position;
+    }
+
+    private void FixedUpdate ()
+    {
+        //mendapatkan posisi untuk kamera
+        Vector3 targetCamPos = target.position + offset;
+
+        //set posisi camera dengan smoothing
+        transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
+        
+    }
+}
